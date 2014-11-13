19df8d843724051e01e697965d66f562ef1152b9
Michael Bochkaryov <misha@rattler.kiev.ua>
2014-11-13 13:44:36 -0800

    Response class initially implemented

--- src/main/java/com/hydra/Response.java
+++ src/main/java/com/hydra/Response.java
@@ -1,5 +1,52 @@
 package com.hydra;
 
+import java.nio.ByteBuffer;
+import java.util.*;
+
 public class Response {
 
+	private int status;
+	private String statusMessage;
+	private Map<String,String> headers; 
+	private ByteBuffer body;
+	
+	public Response() {
+		// Set defaults
+		this.status = 200;
+		this.statusMessage = "OK";
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	public Map<String, String> getHeaders() {
+		return headers;
+	}
+	public void setHeaders(Map<String, String> headers) {
+		this.headers = headers;
+	}
+	public ByteBuffer getBody() {
+		return body;
+	}
+	public void setBody(ByteBuffer body) {
+		this.body = body;
+	}
+	
+	public void setBody(String body) {
+		this.body.put(body.getBytes());
+	}
+	
+	public void setHeader(String name, String value) {
+		this.headers.put(name, value);
+	}
+	
 }
---
+++ src/test/java/com/hydra/ResponseTest.java
@@ -0,0 +1,27 @@
+package com.hydra;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResponseTest {
+
+	private Response res;
+
+	@Before
+	public void setup() {
+		res = new Response();
+	}
+
+	@Test
+	public void defaultStatusIs200() {
+		assertEquals(200, res.getStatus());
+	}
+
+	@Test
+	public void defaultStatusMessageIsOK() {
+		assertEquals("OK", res.getStatusMessage());
+	}
+
+}
