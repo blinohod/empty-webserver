9d32c3e332c610c670b13f68257ff0a1363ba972
Michael Bochkaryov <misha@rattler.kiev.ua>
2014-11-17 14:43:35 -0800

    HTTPSessionTest moved to test directory

--- src/test/java/com/webdaemon/HTTPSessionTest.java
+++ src/test/java/com/webdaemon/HTTPSessionTest.java
@@ -1,23 +1,18 @@
 package com.webdaemon;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class HTTPSessionTest {
 
-	@Before
-	public void setup() {
-		
-	}
 	
 	@Test
 	public void canReadRequestLineFromSocket() throws Exception, IOException {
@@ -25,6 +20,7 @@ public class HTTPSessionTest {
 		ServerSocket listener = new ServerSocket(54321);
 		Socket client = new Socket("127.0.0.1", 54321);
 		Socket server = listener.accept();
+		
 		HTTPSession session = new HTTPSession(server);
 		
 		BufferedWriter out = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));
