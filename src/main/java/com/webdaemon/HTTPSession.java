9069f37c48100c2ae393b287017e77855b52a109
Michael Bochkaryov <misha@rattler.kiev.ua>
2014-11-17 12:06:09 -0800

    Added stub implementations of Handler and HTTPSession

---
+++ src/main/java/com/webdaemon/HTTPSession.java
@@ -0,0 +1,35 @@
+package com.webdaemon;
+
+import java.net.Socket;
+
+public class HTTPSession implements HTTPSessionAPI {
+
+	public HTTPSession(Socket socket) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String readRequestLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String readRequestHeaders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] readRequestBody() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeResponseStatus(String status) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/webdaemon/Handler.java
@@ -0,0 +1,11 @@
+package com.webdaemon;
+
+public class Handler implements HandlerAPI {
+
+	@Override
+	public String processRequest(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
